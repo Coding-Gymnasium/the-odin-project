55ecb4d91f18522cef5d62cec801c90120f1c893
Nico Rithner <nicorithner@gmail.com>
2022-02-10 19:53:59 -0700

    Add scores per round

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -3,26 +3,62 @@ function computerPlay() {
   return ai[Math.floor(Math.random() * 3)];
 }
 
-// let computerSelection = computerPlay().toLowerCase();
-// let playerSelection = prompt('Select Rock, Paper, Scissors').toLowerCase();
-
 function playRound() {
-let computerSelection = computerPlay().toLowerCase();
-let playerSelection = prompt('Select Rock, Paper, Scissors').toLowerCase();
+  let computerSelection = computerPlay().toLowerCase();
+  // let playerSelection = prompt('Select Rock, Paper, Scissors').toLowerCase();
+  let playerSelection = 'rock';
+
   const plays = {
-    rock: { rock: "It's a draw", paper: 'You lose!', scissors: 'You win!' },
-    paper: { rock: 'You win!', paper: "It's a draw", scissors: 'You lose!' },
-    scissors: { rock: 'You lose!', paper: 'You win!', scissors: "It's a draw" },
+    rock: {
+      rock: ["It's a draw", 0, 0],
+      paper: ['You lose!', 0, 1],
+      scissors: ['You win!', 1, 0],
+    },
+    paper: {
+      rock: ['You win!', 1, 0],
+      paper: ["It's a draw", 0, 0],
+      scissors: ['You lose!', 0, 1],
+    },
+    scissors: {
+      rock: ['You lose!', 0, 1],
+      paper: ['You win!', 1, 0],
+      scissors: ["It's a draw", 0, 0],
+    },
   };
 
-  return `Player: ${playerSelection} vs Computer: ${computerSelection}. ${plays[playerSelection][computerSelection]}`;
+  let result = plays[playerSelection][computerSelection];
+  let message = `Player: ${playerSelection} vs Computer: ${computerSelection}. ${result[0]}`;
+  let plScore = result[1];
+  let aiScore = result[2];
+
+  return {
+    message,
+    plScore,
+    aiScore,
+  };
 }
 
 function game() {
-  // 1. run function.
-  // 2. console.log results for the round 
+  let plScore = 0;
+  let aiScore = 0;
+
+  // 1. run function 5 times.
+
+  for (let i = 0; i < 5; i++) {
+    let play = playRound();
+    // 2. console.log results for each round
+    console.log(play.message);
+    plScore += play.plScore;
+    aiScore += play.aiScore;
+    console.log(`Player: ${plScore}, AI: ${aiScore}`);
+  }
+
   // 3. keep score based on round results
+
+  console.log(`Player: ${plScore}, AI: ${aiScore}`);
+
   // 4. reports winner or loser at the end.
+
 }
 
-console.log( game() );
+console.log(game());
