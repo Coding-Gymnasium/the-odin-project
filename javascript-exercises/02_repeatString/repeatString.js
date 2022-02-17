1070a4fab24d8bd99113c0d9b7803a6608bc34be
Nico Rithner <nicorithner@gmail.com>
2022-02-17 16:10:14 -0700

    Complete repeatString exercise

--- javascript-exercises/02_repeatString/repeatString.js
+++ javascript-exercises/02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let concatString = '';
+  if (Math.sign(num) === -1) concatString = 'ERROR';
+  for (let i = 0; i < num; i++) {
+    concatString += string;
+  }
+  return concatString;
 };
 
 // Do not edit below this line
