ddc0da8b21c7f16f0abfff48f9aeb9ef638b26ef
Nico Rithner <nicorithner@gmail.com>
2022-02-18 11:12:33 -0700

    Solve reverseString exercise

--- javascript-exercises/03_reverseString/reverseString.js
+++ javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = (string) => {
+  const arr = string.split(' ');
+  const arrReversed = [];
+  arr.forEach((str) => {
+    arrReversed.push(str.split('').reverse().join(''));
+  });
+  return arrReversed.reverse().join(' ');
 };
 
 // Do not edit below this line
