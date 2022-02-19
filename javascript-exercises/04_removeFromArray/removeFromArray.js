6fc1de8b20e18ee7664917e696d156582367321e
Nico Rithner <nicorithner@gmail.com>
2022-02-19 14:43:46 -0700

    Solve remove from array exercise. Initial solution

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item1, item2, item3, item4) {
+  return array.filter(element => element !== item1 && element !== item2 && element !== item3 && element !== item4)
 };
 
 // Do not edit below this line
