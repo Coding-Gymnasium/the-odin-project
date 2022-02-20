904614e4c9e149a61ccf78ca173abebe1da29801
Nico Rithner <nicorithner@gmail.com>
2022-02-20 13:28:03 -0700

    Add new solution to removeFromArray

--- javascript-exercises/04_removeFromArray/removeFromArray.js
+++ javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function(array, item1, item2, item3, item4) {
-  return array.filter(element => element !== item1 && element !== item2 && element !== item3 && element !== item4)
+const removeFromArray = function (array, ...otherArgs) {
+  let newArr = [...array];
+  for (let i = 0; i < otherArgs.length; i++) {
+    newArr = newArr.filter((item) => item !== otherArgs[i]);
+  }
+    return newArr;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// solution 1.
+// const removeFromArray = function (array, item1, item2, item3, item4) {
+// return array.filter(
+//   (element) =>
+//     element !== item1 &&
+//     element !== item2 &&
+//     element !== item3 &&
+//     element !== item4
+// );
+// };
