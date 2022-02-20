5d18e65df33dd6a4a28de12affd16afb2151bb2d
Nico Rithner <nicorithner@gmail.com>
2022-02-20 14:49:42 -0700

    Solve leapYears

--- javascript-exercises/06_leapYears/leapYears.js
+++ javascript-exercises/06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // condition 1: leapYears are divisible by four.
+  // century years (divisible by 100) are not leapYears
+  // century year divisible by 400 are leapYears
+  if (year % 400 === 0) {
+    return true;
+  } else {
+    return year % 100 != 0 && year % 4 === 0;
+  }
 };
 
 // Do not edit below this line
