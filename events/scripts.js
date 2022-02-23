edafd4c0f26e9df457d355698acdff49f01faa21
Nico Rithner <nicorithner@gmail.com>
2022-02-22 17:41:37 -0700

    Start event lesson

---
+++ events/scripts.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector('#btn');
+
+// Method 2
+// btn.onclick = () => alert('Hello World from js');
+
+// Method 3
+// btn.addEventListener('click', () => alert('Hello Again'));
+
+// using a named function
+function alertFunction() {
+  alert("hi from inside a named function");
+}
+
+function consoleLogFunction() {
+  console.log("You pressed 'a'!");
+}
+
+btn.addEventListener('click', alertFunction);
+
+window.addEventListener('keypress', e => e.code === 'KeyA' && consoleLogFunction())
+
+// The event.
+
+btn.addEventListener('click', function (event) {
+  // console.log(event.target);
+  event.target.style.background = 'pink';
+})
