7983c92164441d953dc40c5196e1953ed211df1f
Nico Rithner <nicorithner@gmail.com>
2022-03-06 18:21:24 -0700

    Add draw while dragging logic

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,13 +1,32 @@
 const grid = document.querySelector('.grid');
 const square = "<div id='square'></div>";
+const sizeSign = document.querySelector('#sizeSign');
 
-let inputedSize = 100;
+let inputedSize = 50;
+let count = 1;
+let pressing = false;
+
+sizeSign.textContent = `GRID: ${inputedSize} x ${inputedSize}`;
 grid.style.width = `${inputedSize * 10}px`;
 grid.style.height = `${inputedSize * 10}px`;
 let size = inputedSize * inputedSize;
-let count = 1;
 
 while (count <= size) {
   grid.insertAdjacentHTML('afterbegin', square);
   count++;
 }
+
+const paint = (e) => {
+  e.preventDefault();
+  grid.addEventListener('mousemove', (e) => {
+    if (pressing) e.target.style.background = 'black';
+  });
+  grid.addEventListener('mouseup', () => {
+    pressing = false;
+  });
+};
+
+grid.addEventListener('mousedown', (e) => {
+  pressing = true;
+  paint(e);
+});
