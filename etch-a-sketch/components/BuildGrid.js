a87982cafbd5ab5ce61297272b407bf5787867d8
Nico Rithner <nicorithner@gmail.com>
2022-03-10 13:26:24 -0700

    Move build grid logic to component

---
+++ etch-a-sketch/components/BuildGrid.js
@@ -0,0 +1,15 @@
+export const BuildGrid = (inputedSize, grid, sizeSign) => {
+  const square = "<div id='square'></div>";
+  let size;
+  let count = 1;
+
+  sizeSign.textContent = `GRID: ${inputedSize} x ${inputedSize}`;
+  grid.style.width = `${inputedSize * 10}px`;
+  grid.style.height = `${inputedSize * 10}px`;
+  size = inputedSize * inputedSize;
+
+  while (count <= size) {
+    grid.insertAdjacentHTML('afterbegin', square);
+    count++;
+  }
+};
