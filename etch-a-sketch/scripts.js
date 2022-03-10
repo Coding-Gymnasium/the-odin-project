2aeb0a6c8583472105dcd86c4b8c7e1332f9feda
Nico Rithner <nicorithner@gmail.com>
2022-03-09 20:32:32 -0700

    Start solution for resizing from user's input

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,26 +1,48 @@
-import {ClearButton} from './components/ClearGrid.js';
+import { ClearButton } from './components/ClearGrid.js';
 
 const grid = document.querySelector('.grid');
 const square = "<div id='square'></div>";
 const sizeSign = document.querySelector('#sizeSign');
+const log = document.getElementById('log');
+let inputedSize = 40;
+let size;
 
-let inputedSize = 50;
+//---- Set Grid Size
+
+sizeSign.textContent = `GRID: ${inputedSize} x ${inputedSize}`;
+
+document.getElementById('setGridSize').addEventListener('click', function (e) {
+  let input = document.getElementById('number'),
+    val = input.value;
+  if (val > '60' || val < '20' || val % 20 != 0) {
+    log.style.display = 'block';
+    input.focus();
+  } else {
+    inputedSize = val;
+    sizeSign.textContent = `GRID: ${inputedSize} x ${inputedSize}`;
+    grid.style.width = `${inputedSize * 10}px`;
+    grid.style.height = `${inputedSize * 10}px`;
+    size = inputedSize * inputedSize;
+  }
+});
+
 let count = 1;
 let pressing = false;
 
-sizeSign.textContent = `GRID: ${inputedSize} x ${inputedSize}`;
 grid.style.width = `${inputedSize * 10}px`;
 grid.style.height = `${inputedSize * 10}px`;
-let size = inputedSize * inputedSize;
+size = inputedSize * inputedSize;
 
 //---- Clear Grid
-ClearButton(grid);
+
+ClearButton(grid, sizeSign, inputedSize);
 
 while (count <= size) {
   grid.insertAdjacentHTML('afterbegin', square);
   count++;
 }
 
+//---- paint
 const paint = (e) => {
   e.preventDefault();
   grid.addEventListener('mousemove', (e) => {
