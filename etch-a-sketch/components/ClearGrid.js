68923f1ffb6728e8aa4ddc8a166fa0f081a30e6e
Nico Rithner <nicorithner@gmail.com>
2022-03-09 20:31:20 -0700

    Add new variables to ClearGrid

--- etch-a-sketch/components/ClearGrid.js
+++ etch-a-sketch/components/ClearGrid.js
@@ -1,10 +1,12 @@
 const clearBtn = document.querySelector('#clear-btn');
 
-export const ClearButton = (grid) => {
+export const ClearButton = (grid, sizeSign, inputedSize) => {
   clearBtn.addEventListener('click', () => {
     let arr = Array.from(grid.querySelectorAll('#square'));
     for (const sq of arr) {
       sq.style.background = 'ivory';
     }
+    sizeSign.textContent = `GRID: 40 x 40`;
+    return inputedSize = 40;
   });
 };
