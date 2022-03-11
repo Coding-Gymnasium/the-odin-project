6db1077f428801191f8b430f5e5e4a67ba2a7d9c
Nico Rithner <nicorithner@gmail.com>
2022-03-10 17:24:40 -0700

    Update styling

--- etch-a-sketch/scripts.js
+++ etch-a-sketch/scripts.js
@@ -1,30 +1,14 @@
 import { ResetSquares } from './components/ResetSquares.js';
 import { PaintSquare } from './components/PaintSquare.js';
 import { BuildGrid } from './components/BuildGrid.js';
+import { ChangeGridSize } from './components/ChangeGridSize.js';
 
 const grid = document.querySelector('.grid');
 const sizeSign = document.querySelector('#sizeSign');
 
-const log = document.getElementById('log');
 let inputedSize = 40;
 
 BuildGrid(inputedSize, grid, sizeSign);
 PaintSquare(grid);
 ResetSquares(grid, sizeSign, inputedSize);
-
-//---- Set Grid Size
-document.getElementById('setGridSize').addEventListener('click', function (e) {
-  let input = document.getElementById('number'),
-    val = input.value;
-  if (val > '60' || val < '20') {
-    log.style.display = 'block';
-    input.focus();
-  } else {
-
-    let sqs = grid.querySelectorAll('#square');
-    sqs.forEach(item => item.remove())
-
-    inputedSize = val;
-    BuildGrid(inputedSize, grid, sizeSign);
-  }
-});
+ChangeGridSize(grid, inputedSize, sizeSign)
