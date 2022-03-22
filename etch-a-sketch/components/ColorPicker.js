de87e5e8a31da8fee31af3ec14be69fca61c6a9f
Nico Rithner <nico@altvia.com>
2022-03-21 23:13:01 -0600

    Add functionality to palette buttons
    Co-authoredi-by: Nico Rithner nicorithner@gmail.com

--- etch-a-sketch/components/ColorPicker.js
+++ etch-a-sketch/components/ColorPicker.js
@@ -1,8 +1,22 @@
-import {PaintSquare} from './PaintSquare.js';
+import { PaintSquare } from './PaintSquare.js';
+
+const colorpicker = document.querySelector('#colorpicker');
+const colorPalette = document.querySelector('.colorPalette');
+const colorButtons = colorPalette.querySelectorAll('.clr-btn');
 
 export const ColorPicker = (grid) => {
-  const colorpicker = document.querySelector('#colorpicker');
   colorpicker.addEventListener('change', () => {
     PaintSquare(grid, colorpicker.value);
+  });
+};
+
+export const ColorButton = (grid) => {
+  colorButtons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      let cb = document.getElementById(btn.id);
+      let compStyles = window.getComputedStyle(cb);
+      let color = compStyles.getPropertyValue('background-color');
+      PaintSquare(grid, color);
+    });
   });
 };
