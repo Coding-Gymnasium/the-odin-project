d22fa516ecbadc1ef5aeacaf37ff7851d20bd687
Nico Rithner <nicorithner@gmail.com>
2022-03-22 21:12:13 -0600

    Solve Calculator exercises

--- javascript-exercises/08_calculator/calculator.js
+++ javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+  //-- Solution 1
+  // const initialValue = 0;
+  // const sum = arr.reduce(
+  //   (previous, current) => previous + current,
+  //   initialValue
+  // );
+  // return sum;
+
+  //-- Solution 2
+  // let sum = arr.reduce((previous, current) => previous + current, 0)
+  // return sum;
+
+  //-- Solution 3
+  let sum = arr.reduce(function (previous, current) {
+    return previous + current;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  let mult = arr.reduce((previous, current) => previous * current, 1)
+  return mult;
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+  return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  let facto = 1;
+  if (num != 0 || num != 1) {
+    for(let i = num; i >= 1; i--) {
+      facto *= i;
+    }
+  }
+  return facto;
 };
 
 // Do not edit below this line
@@ -29,5 +52,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
