1857b6c9642888a17457879ea77a8456fd708594
Nico Rithner <nicorithner@gmail.com>
2022-04-03 12:05:45 -0600

    Add checkForZero function

--- calculator/modules/UpdateDisplay.js
+++ calculator/modules/UpdateDisplay.js
@@ -1,45 +1,51 @@
 import { Calculations } from './Calculations.js';
 
-const history = document.querySelector('#previous-operand');
-const currOperand = document.querySelector('#current-operand');
-const historyArr = [];
+const currOperand = document.querySelector('.output');
 let currOperandArr = [];
 let set = [];
 let calculation;
 const operations = ['+', '−', '×', '÷', '='];
 
+
 export const UpdateDisplay = (key) => {
   if (operations.includes(key)) {
-    //---> checks for 0s on left side
-    if (currOperandArr[0] == '0') {
-      currOperandArr.shift();
-    }
-
     //---> don't add operation if there are no number in the current operand display
-    if (!currOperandArr[0]) return;
+    if (!currOperandArr[0]) {
+      alert('Enter a number first');
+      return;
+    }
+    //---> checks for 0s on left side
+    checkForZero();
 
-    let numberString = currOperandArr.join('');
-    set.push(numberString);
+    // build set array to run calculation
+    set.push(currOperandArr.join(''));
     set.push(key);
+
     if (set.length > 2) {
       calculation = Calculations(set[0], set[1], set[2]);
-      historyArr.push(calculation);
-      console.log(historyArr);
+      set = [calculation];
+      if (key != '=') set.push(key);
     }
 
     //---> Displays a string to show on current operand display
     currOperand.textContent = calculation;
 
-    //---> Creates a string to show on top display
-    let input = `${history.textContent} ${numberString} ${key}`;
-    history.textContent = input;
-    // currOperand.textContent = '0';
-    currOperandArr = ['0'];
+    currOperandArr = [];
   } else {
-    if (currOperandArr[0] == '0') {
-      currOperandArr.shift();
-    }
+    checkForZero();
     currOperandArr.push(key);
     currOperand.textContent = currOperandArr.join('');
   }
 };
+
+const checkForZero = () => {
+  if (currOperandArr[0] == '0') {
+    currOperandArr.shift();
+  }
+};
+
+// when I enter a number it shows in the bottom display.
+// when I press an operation sign following a number, the button gets highlighted with a border.
+// when I add another number and
+//  - press equal, the result replaces the existing number.
+//  - press an operation button, the result replaces the existing number and the button gets highlighted with a border.
