9785b47a40f97b1364bc135ed48b9ef2791b07df
Nico Rithner <nicorithner@gmail.com>
2022-04-04 22:38:57 -0600

    Create functions for clearAll and clearLast

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,3 @@
 import { GetKeys } from './modules/GetKeys.js';
 
 GetKeys();
-
-// ---- Clear buttons
-
-// const clearLast = () => {
-//   clr.addEventListener('mousedown', () => {
-//     currOperand.textContent = 0;
-//     prevOperand.textContent = prevOperand.textContent.slice(0, -1);
-//   });
-// };
-//
-// clearLast();
