d00c92248d6b4ad2519dfa18a9ecc077bff27a79
Nico Rithner <nicorithner@gmail.com>
2022-04-07 17:45:22 -0600

    Limit scope of variables
    Sort variables separating local and
    DOM variables from each other

--- calculator/modules/UpdateDisplay.js
+++ calculator/modules/UpdateDisplay.js
@@ -1,41 +1,54 @@
 import { Calculations } from './Calculations.js';
 
 const currOperand = document.querySelector('.output');
-let currOperandArr = [];
-let set = [];
-let calculation;
-let input;
-const operations = ['+', '−', '×', '÷', '='];
 const clr = document.querySelector('.clear');
 const clrAll = document.querySelector('.clearAll');
 
+const currOperandArr = [];
+const set = [];
+const result = [];
+const operations = ['+', '−', '×', '÷', '='];
+
 export const UpdateDisplay = (key) => {
-  input = currOperandArr.join('');
+  console.log('key: ', key);
   if (operations.includes(key)) {
+    let calculation;
+    let input;
+
     //---> don't add operation if there are no number in the current operand display
     if (!currOperandArr[0]) {
       alert('Enter a number first');
       return;
     }
-
     //---> checks for 0s on left side
     checkForZero();
 
     // build set array to run calculation
+    input = currOperandArr.join('');
     set.push(input);
     set.push(key);
 
+    // run calculation if complete set
     if (set.length > 2) {
       calculation = Calculations(set[0], set[1], set[2]);
-      set = [calculation];
+      set.splice(0, set.length, calculation); // replace set values with calculation value
+      result.splice(0, result.length, calculation); // replace result value with calculation value
+
+      // only push key into set if it is not '='
       if (key != '=') set.push(key);
+
+      console.log('input: ', input);
+      console.log('set: ', set);
+      console.log('result: ', result);
+      console.log('currOperandArr: ', currOperandArr);
+      console.log('currOperand: ', currOperand);
     }
 
     //---> Displays a string to show on current operand display
     calculation
       ? (currOperand.textContent = calculation)
       : (currOperand.textContent = input);
-    currOperandArr = [];
+    currOperandArr.length = 0;
   } else if (key === clr.textContent) {
     clearLast();
   } else if (key === clrAll.textContent) {
@@ -55,7 +68,9 @@ const checkForZero = () => {
 
 const clearAll = () => {
   currOperand.textContent = 0;
-  currOperandArr = [];
+  currOperandArr.length = 0;
+  result.length = 0;
+  set.length = 0;
 };
 
 const clearLast = () => {
